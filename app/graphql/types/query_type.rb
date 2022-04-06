57a614e509202fb58e1e97dd9871ec8de9fb6545
Artem Popkov <artem.popkov@itechart-group.com>
2022-04-07 01:16:35 +0300

    fix

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -1,11 +1,17 @@
 module Types
   class QueryType < Types::BaseObject
-    field :games, [Types::GameType], 'Find all games', null: true
-    field :game, Types::GameType, 'Find a game by ID', null: true do
+    field :games, [Types::GameType], "Find all games", null: true
+    field :game, Types::GameType, "Find a game by ID", null: true do
       argument :id, ID, required: true
     end
-    
-    def games
+
+    field :games_connection, Types::GameType.connection_type, null: false 
+   
+    def games_connection(**_args)
+      Game.all
+    end
+
+    def games(**_args)
       Game.all
     end
 
